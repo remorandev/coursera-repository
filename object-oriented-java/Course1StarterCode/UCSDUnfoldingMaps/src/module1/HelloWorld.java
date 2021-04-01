20939d32b1b49ed1684037566010570198b7cee0
Roberto Moran <remorandev@gmail.com>
2021-04-02 00:05:48 +0200

    Practice Exercises Module 2
     Changes to be committed:
    	modified:   src/module1/HelloWorld.java

--- object-oriented-java/Course1StarterCode/UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ object-oriented-java/Course1StarterCode/UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -1,5 +1,6 @@
 package module1;
 
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import processing.core.PApplet;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
@@ -46,7 +47,7 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		AbstractMapProvider provider = new OpenStreetMap.OpenStreetMapProvider();
 		// Set a zoom level
 		int zoomLevel = 10;
 		
@@ -66,16 +67,15 @@ public class HelloWorld extends PApplet
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
 		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map2 = new UnfoldingMap(this, 410, 50, 350, 500, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
-	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-		
+	    map1.zoomAndPanTo(zoomLevel, new Location(-25.7f, -80.2f));
+	    map2.zoomAndPanTo(zoomLevel, new Location(-33.9f, 18.4f));
+
 		// This line makes the map interactive
-		MapUtils.createDefaultEventDispatcher(this, map1);
-		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
+		MapUtils.createDefaultEventDispatcher(this, map1, map2);
 
 	}
 
@@ -84,6 +84,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
